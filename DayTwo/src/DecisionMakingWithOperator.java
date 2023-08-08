0ff599de1b8f6896042be9b56062421e87e177b5
HP <HP@LAPTOP-U2168N1P>
2023-08-08 16:12:19 +0530

    first commit

---
+++ DayTwo/src/DecisionMakingWithOperator.java
@@ -0,0 +1,13 @@
+
+public class DecisionMakingWithOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int x=8;
+      int y=7;
+      int a=10;
+      int b=11;
+      
+	}
+
+}
---
+++ DayTwo/src/OperatorsDemo.java
@@ -0,0 +1,18 @@
+
+public class OperatorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=30;
+		System.out.println(a+" "+b+" "+c);
+		String d=2+"4";
+		int e=2+4;
+		String s="4"+"3";
+		System.out.println(d+""+e+"");
+		
+
+	}
+
+}
